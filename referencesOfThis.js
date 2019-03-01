b293a2ff12c3f4cab0175a9878698c5efb20a9c4
HaraHiroshi <mrbob403@gmail.com>
2019-03-02 01:28:32 +0900

    update referencesOfThis.js

--- referencesOfThis.js
+++ referencesOfThis.js
@@ -1,5 +1,5 @@
 // thisの参照先テスト
-var thisValue = 'this is this. is';
+var thisValue = 'this is grobalValue';
 
 let testClass = function(thisValue) {
   // 1.コンストラクタ...　生成されるインスタンス自身
@@ -23,3 +23,28 @@ console.log(showThisValue()); // this is grobalValue/Strictモードではerror
 // 4.メソッド( == インスタンスのメンバ関数)... 呼び出し元のオブジェクト
 var tC1 = new testClass('this is Object\'s property');
 console.log(tC1.getThisValue()); // this is Object's property
+
+// 5.callメソッド... 引数で指定されたオブジェクトのプロパティ
+// 関数オブジェクト.call(関数の中でthisが示すもの, 関数に渡す引数(可変長引数));
+function callTest(value) {
+  console.log('this=' + this);
+  console.log('引数=' + value);
+}
+var data = 'test';
+callTest.call(data, 'ひきすう');
+
+// 6.applyメソッド... 引数で指定されたオブジェクトのプロパティ
+// 関数オブジェクト.apply(関数の中でthisが示すもの, 関数に渡す引数(配列));
+// なお、関数オブジェクトでは既にargumentsオブジェクトを持つので仮引数を用意する必要はない
+function applyTest() {
+  for (var i = 0; i < arguments.length; i++) {
+    arguments[i] += this;
+  }
+  return arguments;
+}
+var data = 100;
+var array = [1, 2, 3, 4, 5];
+var result = applyTest.apply(data, array);
+for (var i = 0; i < result.length; i++) {
+  console.log(result[i]);
+}
