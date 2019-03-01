431cc37e8aa20c62a8514f2c7e7f354aaedb43dc
HaraHiroshi <mrbob403@gmail.com>
2019-03-02 00:10:57 +0900

    update objectOrientedArchtechture.js

--- objectOrientedArchtechture.js
+++ objectOrientedArchtechture.js
@@ -1,26 +1,47 @@
 'use strict';
 // JavaScriptでは関数(Functionオブジェクト)にクラスとしての役割を与えている
-let firstName;
-let lastName;
-let age;
-let height;
-let weight;
-
 var Person = function(firstName, lastName, age, height, weight) {
+
   this.firstName = firstName;
   this.lastName = lastName;
   this.age = age;
   this.height = height;
   this.weight = weight;
-}
 
-function getFirstName() {
-  return this.firstName;
-}
+  this.getName = function() {
+    return this.firstName + ' ' + this.lastName;
+  }
+  this.getAge = function() {
+    return this.age;
+  }
+  this.getHeight = function() {
+    return this.height;
+  }
+  this.getWeight = function() {
+    return this.weight;
+  }
+  // オブジェクトを凍結することでメンバの追加/削除を防止できる
+  // Object.seal(this);
+};
 
-function getLastName() {
-  return this.lastName;
+// JavaScriptでは同一のクラスから生成されたインスタンスであっても、
+// それぞれが持つメンバは同一であるとは限らない
+var p1 = new Person('Chris', 'Coleman', 40, 180, 90);
+var p2 = new Person('Dave', 'Weckle', 50, 170, 70);
+console.log(p1.getName());
+console.log(p2.getName());
+// p1のメンバにdisplay()を追加
+p1.display = function() {
+  console.log('---------------');
+  console.log('NAME:' + this.getName());
+  console.log('AGE:' + this.getAge());
+  console.log('HEIGHT:' + this.getHeight());
+  console.log('WEIGHT:' + this.getWeight());
+  console.log('---------------');
 }
+p1.display();
+// p2.display(); p2のメンバにはdisplay()が定義されていない
 
-var p = new Person('Hiroshi', 'Hara', 27, 168.7, 56.2);
-console.log(p.firstName);
+delete p1.getName;
+console.log(p2.getName()); // p1からメンバを削除してもp2に影響はない
+// console.log(p1.getName());　error
